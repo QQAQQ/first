4c472f4ec2defc2d99e4e9c567a0699fe8cb9835
Administrator <gfdgf@ad>
2017-07-12 15:22:09 +0800

    sum(7,8)

--- git/src/git/Ta.java
+++ git/src/git/Ta.java
@@ -2,10 +2,10 @@ package git;
 
 public class Ta {
 	int x= 0;
-	public int sum(int x ,int y){
+	public static  int sum(int x ,int y){
 		return x+y;
 	}
 	public static void main(String[] args) {
-		
+		sum(7, 8);
 	}
 }
